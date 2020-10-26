30365569ba24950d68c27412cc0b44cb0af63b87
Anna <rosalenanna@gmail.com>
2020-10-26 16:47:14 +0100

    script

---
+++ js/myscript.js
@@ -0,0 +1,23 @@
+
+
+
+// Scrivi un programma che stampi i numeri da 1 a 100,
+for (var i=1; i<=100; i++){
+
+    // ma per i multipli di 3 stampi “Fizz” al posto del numero
+  if (i % 3 === 0 && i % 5 === 0){
+    console.log("FizzBuzz");
+
+    // e per i multipli di 5 stampi “Buzz”.
+  } else if (i % 5 === 0){
+    console.log("Buzz");
+
+    // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”
+  }else if (i % 3 === 0){
+    console.log("Fizz");
+
+  }else{
+    console.log(i);
+  }
+
+}
